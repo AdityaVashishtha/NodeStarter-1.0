b6df42a5c94d05d46fbafcf6ffb03236c672d525
Aditya Vashishtha <vashishthaaditya20@gmail.com>
2017-12-06 15:25:28 +0530

    Bulma CSS as front end framework added and Pug as view engine Login and sign up front end done

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require("express")
 const path = require('path')
-const mustache = require('mustache')
 const config = require("./config")
 const users = require("./routes/users")
-const fs = require('fs')
 
 
 const app = express()
@@ -11,21 +9,13 @@ const app = express()
 // Set  :: Public folder as default static path
 app.use(express.static(path.join(__dirname,'public')));
 
-// Set :: View Engine
-app.engine('html', function (filePath, options, callback) { 
-    fs.readFile(filePath, function (err, content) {
-        if (err)
-            return callback(err)        
-        var rendered = mustache.to_html(content.toString(),options);
-        return callback(null, rendered)
-    })
-  })
+// Set :: View Engine Mustache
 app.set('views',path.join(__dirname,'views'));
-app.set('view engine','html');
+app.set('view engine','pug');
 
 // Index Page default Route set
 app.get('/',(req,res)=>{        
-    res.render('index',{title:'Index Page'});
+    res.render('index',{title:'Home'});
 });
 
 
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ router.get('/',(req,res)=>{
     res.send('USERS HOME PAGE');
 });
 
+router.get('/login',(req,res)=>{
+    res.render('login',{title:'Login'});
+});
+
+router.get('/signup',(req,res)=>{
+    res.render('signup',{title:'Sign Up'});
+});
+
+router.post('/login',(req,res)=>{
+    res.send('Login authenticate');
+});
+
 module.exports = router
